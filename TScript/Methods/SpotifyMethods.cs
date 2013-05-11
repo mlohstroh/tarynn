1574615b7802993d9943c72f8014ffca85b5c1cc
Mark Lohstroh <mlohstroh@gmail.com>
2013-05-11 15:27:41 -0500

    Going to add support for spotify

---
+++ TScript/Methods/SpotifyMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spotify;
+
+namespace TScript.Methods
+{
+    public class SpotifyMethods : MethodPackage
+    {
+        public SpotifyMethods() 
+            : base("Spotify Methods")
+        {
+        }
+
+        public override TObject GetNewObjectForType(string type, string name)
+        {
+            return TObject.Empty(); 
+        }
+
+        public override TObjectChange GetResultForMethod(string method, object[] args)
+        {
+            return TObjectChange.Empty();
+        }
+    }
+}
