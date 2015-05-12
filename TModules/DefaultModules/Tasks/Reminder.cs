897411df771b1674ecd264b015c931f93bf5767a
Mark Lohstroh <mlohstroh@gmail.com>
2015-05-12 13:49:37 -0500

    missing files from list

---
+++ TModules/DefaultModules/Tasks/Reminder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LitJson;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TModules.DefaultModules.Tasks
+{
+    [BsonIgnoreExtraElements]
+    public class Reminder
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement]
+        public string Title { get; set; }
+
+        [BsonElement]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime Due { get; set; }
+
+        public Reminder(string name, DateTime due)
+        {
+            Title = name;
+            Due = due;
+        }
+    }
+}
---
+++ TModules/DefaultModules/Tasks/TaskList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TModules
+{
+    [BsonIgnoreExtraElements]
+    public class TaskList
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement]
+        public string ListTitle { get; set; }
+
+        [BsonElement]
+        public List<string> Items { get; set; }
+
+        public TaskList (string title, List<string> items)
+        {
+            ListTitle = title;
+            Items = items;
+        }
+
+        public TaskList() { }
+    }
+}
+
