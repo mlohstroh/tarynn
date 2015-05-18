48571de238dfb0e32b7ed33315a9d596f339dbc6
Mark Lohstroh <mlohstroh@gmail.com>
2015-05-18 02:13:20 -0500

    working on the route class

--- TRouter/Router/Route.cs
+++ TRouter/Router/Route.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Net;
 
 namespace TRouter
 {
     public class Route
     {
-        public Route ()
+        public ParameterString ParameterString { get; private set; }
+        public Func<HttpWebRequest, HttpWebResponse, HttpWebResponse> DelegateFunction { get; private set; }
+
+        public Route (string url, Func<HttpWebRequest, HttpWebResponse, HttpWebResponse> func)
         {
+            ParameterString = new ParameterString(url);
+            DelegateFunction = func;
         }
     }
 }
---
+++ TRouterTests/RouteTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRouter;
+using Xunit;
+
+namespace TRouterTests
+{
+    public class RouteTests
+    {
+        [Fact]
+        public void PropertiesOnRouteShouldBeValid()
+        {
+            Route r = new Route("/url", (request, response) => null);
+            Assert.True(r.ParameterString != null);
+            Assert.True(r.DelegateFunction != null);
+        }
+    }
+}
